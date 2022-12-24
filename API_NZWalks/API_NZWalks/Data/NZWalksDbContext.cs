6c0cb6d30207598ed8cf9401342ccc3c3e27023e
rajatgudi <rajatgudigar123@gmail.com>
2022-12-24 12:38:33 +0530

    Initial Migration and EF

---
+++ API_NZWalks/API_NZWalks/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API_NZWalks.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_NZWalks.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options):base(options) 
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalksDifficulty { get; set; }
+    }
+}
